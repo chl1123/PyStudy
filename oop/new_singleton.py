79a928ac028770ad4b1e8884628d133bceb7257f
chenHongli <chenhl@seer-group.com>
2025-03-19 09:27:22 +0800

    ~ add: 修复单例模式

--- oop/new_singleton.py
+++ oop/new_singleton.py
@@ -3,14 +3,20 @@ import threading
 
 class Singleton(object):
     _instance_lock = threading.Lock()
+    _initialized = False  # 是否初始化完成
 
     def __init__(self):
-        pass
+        # 对象的初始化: 对__new__返回的对象进行初始化, 设置属性、执行逻辑
+        if not Singleton._initialized:
+            print("Singleton init")
+            Singleton._initialized = True
 
     def __new__(cls, *args, **kwargs):
+        # 创建类的实例: 分配内存, 返回新对象
         if not hasattr(Singleton, "_instance"):
             with Singleton._instance_lock:
                 if not hasattr(Singleton, "_instance"):
+
                     Singleton._instance = object.__new__(cls)
         return Singleton._instance
 
