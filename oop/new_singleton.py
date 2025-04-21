3d8011fa173735f9cb89482c7e02e6e911cca460
chenHongli <chenhl@seer-group.com>
2025-04-21 11:16:46 +0800

    + add: 线程内每个标识每个单例

--- oop/new_singleton.py
+++ oop/new_singleton.py
@@ -5,33 +5,61 @@ class Singleton(object):
     _instance_lock = threading.Lock()
     _initialized = False  # 是否初始化完成
 
+    def __new__(cls, *args, **kwargs):
+        # 创建类的实例: 分配内存, 返回新对象
+        if not hasattr(Singleton, "_instance"):
+            with Singleton._instance_lock:
+                if not hasattr(Singleton, "_instance"):
+                    Singleton._instance = object.__new__(cls)
+        return Singleton._instance
+
     def __init__(self):
         # 对象的初始化: 对__new__返回的对象进行初始化, 设置属性、执行逻辑
         if not Singleton._initialized:
             print("Singleton init")
             Singleton._initialized = True
 
-    def __new__(cls, *args, **kwargs):
-        # 创建类的实例: 分配内存, 返回新对象
-        if not hasattr(Singleton, "_instance"):
-            with Singleton._instance_lock:
-                if not hasattr(Singleton, "_instance"):
+class MutilSingleton:
+    _instances = {}
+    _instance_lock = threading.Lock()
 
-                    Singleton._instance = object.__new__(cls)
-        return Singleton._instance
+    def __new__(cls, id_, *args, **kwargs):
+        if id_ not in MutilSingleton._instances:
+            with MutilSingleton._instance_lock:
+                if id_ not in MutilSingleton._instances:
+                    instance = object.__new__(cls)
+                    MutilSingleton._instances[id_] = {"instance": instance, "init": False}
+        return MutilSingleton._instances[id_]["instance"]
 
+    def __init__(self, id_):
+        if self._instances[id_]["init"] is False:
+            print(id_ + " init")
+            self._instances[id_]["init"] = True
+            self.client = "client_" + id_
 
-# 类() 先执行__new__方法，再执行__init__方法
-obj1 = Singleton()
-obj2 = Singleton()
-print(obj1, obj2)
+    def get_client(self):
+        return self.client
 
+def singleton_example():
+    # 类() 先执行__new__方法，再执行__init__方法
+    obj1 = Singleton()
+    obj2 = Singleton()
+    print(obj1, obj2)
 
-def task(arg):
-    obj = Singleton()
-    print(obj)
+    def task(arg):
+        obj = Singleton()
+        print(obj)
 
+    for i in range(10):
+        t = threading.Thread(target=task, args=[i, ])
+        t.start()
 
-for i in range(10):
-    t = threading.Thread(target=task, args=[i, ])
-    t.start()
+def mutil_singleton_example():
+    obj1 = MutilSingleton("1")
+    obj2 = MutilSingleton("1")
+    obj3 = MutilSingleton("2")
+    print(obj1, obj2, obj3)
+    print(obj1.get_client(), obj2.get_client(), obj3.get_client())
+
+if __name__ == '__main__':
+    mutil_singleton_example()
